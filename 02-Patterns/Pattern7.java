fb936533d0ea17dd16339ef4f762afe8c31b9c8d
Piyush Shukla <piyushshukla8980@gmail.com>
2026-08-14 06:53:42 +0530

    Build the erect and inverted logic myslef and also used them in the diamond pattern problem

--- 02-Patterns/Pattern7.java
+++ 02-Patterns/Pattern7.java
@@ -19,8 +19,8 @@
 public class Pattern7{
     public static void main(String[]args){
         int n=5;
-        for(int i=0;i<=n;i++){
-            for (int j=0;j<=n-i-1;j++){
+        for(int i=0;i<n;i++){
+            for (int j=0;j<n-i-1;j++){
                 System.out.print(" ");
             }
 
