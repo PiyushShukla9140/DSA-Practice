239342cb152e62412f011303f01ceb496524a4d3
Piyush Shukla <piyushshukla8980@gmail.com>
2026-08-16 10:08:26 +0530

    4 more pattern ques completed

--- 02-Patterns/Pattern12.java
+++ 02-Patterns/Pattern12.java
@@ -1,12 +1,21 @@
 public class Pattern12{
     public static void main(String[]args){
         int n=4;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<i+1;j++){
-                System.out.print(i);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+                
+            }
+            for(int j=1;j<=2*n-(2*i);j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
                 
             }
             System.out.println();
+            
+
         }
     }
 }
---
+++ 02-Patterns/Pattern15.java
@@ -0,0 +1,11 @@
+public class Pattern15{
+    public static void main(String[]args){
+        int n=5;
+        for(int i=n;i>0;i--){
+            for(char ch='A';ch<'A'+i;ch++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+}
